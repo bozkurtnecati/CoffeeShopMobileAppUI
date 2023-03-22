e975d1d399c9dd24d53d89d5d0f5e3ef79f09923
bozkurtnecati <bozkurtnecati07@hotmail.com>
2023-03-22 14:56:38 +0300

    Initial Commit

---
+++ CoffeeShopMobileAppUI/CoffeeShopMobileAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopMobileAppUIApp.swift
+//  CoffeeShopMobileAppUI
+//
+//  Created by Necati Bozkurt on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopMobileAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
